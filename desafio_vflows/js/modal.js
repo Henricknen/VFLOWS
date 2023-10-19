476e4bad8d97e86c3725de19973123163e944e15
Luis Henrique Siva Ferreira <l.henrick@live.com>
2023-10-18 23:56:07 -0300

    criando_um_'modal'

---
+++ desafio_vflows/js/modal.js
@@ -0,0 +1,6 @@
+document.getElementById("save-button").addEventListener("click", function () {
+    // Exibe o modal de carregamento
+    document.getElementById("loading-modal").style.display = "block";
+  
+    // Outras ações, como enviar dados para o servidor, podem ser adicionadas aqui.
+  });
